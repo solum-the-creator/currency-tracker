f7f5a78d9ca2c7ce7af5debfcb125db419c9bb3c
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-09-12 19:50:33 +0300

    feat: create header layout

---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,35 @@
+import { Logo } from '@components/ui/Logo';
+import { Toggle } from '@components/ui/Toggle';
+import { NAV_LINKS } from '@constants/paths';
+import { NavLink } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.content_wrapper}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.nav_list}>
+            {NAV_LINKS.map((link) => (
+              <li key={link.path} className={styles.nav_item}>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) => (isActive ? styles.active : '')}
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.theme_control}>
+          <Toggle />
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -4,3 +4,22 @@ export enum UrlPaths {
   BANK_CARD = '/bank-card',
   CONTACT = '/contact',
 }
+
+export const NAV_LINKS = [
+  {
+    name: 'Home',
+    path: UrlPaths.HOME,
+  },
+  {
+    name: 'Timeline',
+    path: UrlPaths.TIMELINE,
+  },
+  {
+    name: 'Bank card',
+    path: UrlPaths.BANK_CARD,
+  },
+  {
+    name: 'Contact',
+    path: UrlPaths.CONTACT,
+  },
+];
