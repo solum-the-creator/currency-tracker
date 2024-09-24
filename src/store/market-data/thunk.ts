c26284ea5958da1778626094f9e3d1d713996869
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-09-24 10:31:22 +0300

    refactor: add func for creating api url

--- src/store/market-data/thunk.ts
+++ src/store/market-data/thunk.ts
@@ -1,21 +1,19 @@
 import { FetchMarketDataParams, MarketData, MarketDataResponse } from '@customTypes/market';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getMarketApiUrl } from '@utils/urlUtils';
 import axios from 'axios';
 
 export const fetchMarketData = createAsyncThunk<MarketData[], FetchMarketDataParams>(
   'marketData/fetchMarketData',
   async ({ currencyCode, startDate, endDate }) => {
-    const response = await axios.get<MarketDataResponse[]>(
-      `${process.env.COIN_API_BASE_URL}/exchangerate/${currencyCode}/USD/history`,
-      {
-        params: {
-          apikey: process.env.COIN_API_KEY,
-          period_id: '1DAY',
-          time_start: startDate,
-          time_end: endDate,
-        },
+    const response = await axios.get<MarketDataResponse[]>(getMarketApiUrl(currencyCode), {
+      params: {
+        apikey: process.env.COIN_API_KEY,
+        period_id: '1DAY',
+        time_start: startDate,
+        time_end: endDate,
       },
-    );
+    });
 
     const marketData: MarketData[] = response.data.map((item) => ({
       time_open: item.time_open,
--- src/utils/urlUtils.ts
+++ src/utils/urlUtils.ts
@@ -1,1 +1,6 @@
+import { CurrenciesCode } from '@customTypes/currency';
+
 export const getApiUrl = (endpoint: string) => `${process.env.API_BASE_URL}/${endpoint}`;
+
+export const getMarketApiUrl = (currencyCode: CurrenciesCode) =>
+  `${process.env.COIN_API_BASE_URL}/exchangerate/${currencyCode}/USD/history`;
