3cfa59672e704537cb02199dd1dfcedde5d407ed
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-09-26 11:42:30 +0300

    feat: implement hiding the list in footer

--- src/components/layout/Footer/LinkRow/index.tsx
+++ src/components/layout/Footer/LinkRow/index.tsx
@@ -1,3 +1,6 @@
+import cn from 'classnames';
+import { useState } from 'react';
+
 import styles from './index.module.scss';
 
 type LinkRowProps = {
@@ -5,10 +8,18 @@ type LinkRowProps = {
   linkList: Array<{ to: string; label: string }>;
 };
 export const LinkRow: React.FC<LinkRowProps> = ({ title, linkList }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.linkRow}>
-      <h4 className={styles.linkTitle}>{title}</h4>
-      <div className={styles.links}>
+      <button onClick={toggleOpen} type="button" className={styles.buttonTitle}>
+        <h4 className={styles.linkTitle}>{title}</h4>
+      </button>
+      <div className={cn(styles.links, isOpen && styles.open)}>
         {linkList.map((link) => (
           <a
             key={link.to}
